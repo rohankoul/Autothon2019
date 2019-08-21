9b5e29dcde2eb449d11c2295c3c113f35f0b2379
harihara <harihara.prasad.vaidyanath@sap.com>
2019-08-21 12:26:29 +0530

    with flow and utilities

--- src/main/java/autothon/autothon/helper/SeleniumUtilities.java
+++ src/main/java/autothon/autothon/helper/SeleniumUtilities.java
@@ -1,13 +1,16 @@
 package autothon.autothon.helper;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class SeleniumUtilities {
 	
+	static String loc = "C:\\Users\\i340909\\git\\Autothon2019\\src\\main\\resources\\screenshot\\";
 	
 	public Boolean isEnabled(WebElement element,String xpath) {
 		return element.isEnabled();
@@ -27,12 +30,13 @@ public class SeleniumUtilities {
 			WebElement e = driver.findElement(By.xpath(xpath));
 			e.clear();
 			e.sendKeys(value);
+			Screenshot.takeScreenshot(driver, loc+screenshot_name);
 			html.insertRow("Input", value, "", 1);
 			System.out.println("Entered"+value);
 		}catch(Exception e) {
 			
-			String loc = "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name;
-			Screenshot.takeScreenshot(driver, "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name);
+			//String loc = "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name;
+			Screenshot.takeScreenshot(driver, loc+screenshot_name);
 			System.out.println("Exception in enter input method -> xpath = "+xpath);
 			html.insertRow("Input",e.getMessage(),loc, 0);
 			throw new SeleniumScriptFailedException (e);
@@ -40,6 +44,27 @@ public class SeleniumUtilities {
 			
 		}
 		
+	}
+	
+	public static void mouseHover(WebDriver driver, WebElement videoScreen, WebElement settingsQuality, WebElement resolutionPath)
+	{
+		//Instantiate Action Class        
+	    Actions actions = new Actions(driver);
+	    //Mouse hover video screen
+	    actions.moveToElement(videoScreen).perform();
+	    System.out.println("Done Mouse hover");
+	 
+	    //Now Select 'Settings' from sub menu which has got displayed on mouse hover of 'Video Screen'
+	    //Mouse hover menuOption 'Settings'
+	    actions.moveToElement(settingsQuality).perform();
+	    System.out.println("Done Mouse hover on 'Settings' ");
+	 
+	    //Now , finally, it displays the desired menu list from which required option needs to be selected
+	    WebElement selectMenuOption = driver.findElement(By.xpath(".//div[contains(text(),'Alternative')]"));
+	    settingsQuality.click();
+	    System.out.println("Selected 'settings' from Video");
+	    resolutionPath.click();
+
 	}
 	
 	public void enterInputAndSubmit(WebDriver driver,String xpath, String value,String screenshot_name, HTMLOutputWriter html) throws SeleniumScriptFailedException
@@ -53,11 +78,12 @@ public class SeleniumUtilities {
 			e.sendKeys(value);
 			e.submit();
 			html.insertRow("Input & Submit",value, "", 1);
+			Screenshot.takeScreenshot(driver, loc+screenshot_name);
 			System.out.println("Entered & submitted"+value);
 		}
 		catch(Exception e) {
-			String loc = "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name;
-			Screenshot.takeScreenshot(driver, "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name);
+		//	String loc = "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name;
+			Screenshot.takeScreenshot(driver, loc+screenshot_name);
 			html.insertRow("Input & Submit",e.getMessage(), loc, 0);
 			System.out.println("Exception in enter input and submit method -> xpath = "+xpath);
 			throw new SeleniumScriptFailedException (e);
@@ -69,6 +95,75 @@ public class SeleniumUtilities {
 	}
 	
 	
+	public void scrollDown(WebDriver driver, WebElement element)
+	{
+		 JavascriptExecutor js = (JavascriptExecutor) driver;
+		//To maximize the window.		
+	     driver.manage().window().maximize();
+	   //This will scroll the page till the element is found		
+	     js.executeScript("arguments[0].scrollIntoView();", element);
+
+	}
+	
+	public WebElement findElement(WebDriver driver,String xpath) throws SeleniumScriptFailedException {
+		try {
+			//WebDriverWait wait = new WebDriverWait(driver, TIMEOUT_WAIT_UNTIL);
+			//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+			WebElement e = driver.findElement(By.xpath(xpath));
+			//e.click();
+			//html.insertRow("Click", xpath, "", 1);
+			System.out.println("Found "+xpath+" Sucessfully");
+			//Screenshot.takeScreenshot(driver, loc+screenshot_name);
+			return e;
+			
+		}
+		catch(Exception ex)
+		{
+			//String loc = "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name;
+			System.out.println("Exception in enter find and click element method -> xpath = "+xpath);
+			//Screenshot.takeScreenshot(driver, "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name);
+			//html.insertRow("Click", ex.getMessage(),loc, 0);
+			//Screenshot.takeScreenshot(driver, loc+screenshot_name);
+			
+			
+			throw new SeleniumScriptFailedException (ex);
+			
+		}finally {
+			
+		}
+		
+	}
+	
+	public WebElement clickElement(WebDriver driver, WebElement e,String screenshot_name) throws SeleniumScriptFailedException {
+		try {
+			//WebDriverWait wait = new WebDriverWait(driver, TIMEOUT_WAIT_UNTIL);
+			//wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(xpath)));
+			e.click();
+			//html.insertRow("Click", xpath, "", 1);
+			System.out.println("Click on Sucessfully");
+			Screenshot.takeScreenshot(driver, loc+screenshot_name);
+			
+			return e;
+			
+			
+		}
+		catch(Exception ex)
+		{
+			//String loc = "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name;
+			//System.out.println("Exception in enter find and click element method -> xpath = "+xpath);
+			//Screenshot.takeScreenshot(driver, "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name);
+			//html.insertRow("Click", ex.getMessage(),loc, 0);
+			Screenshot.takeScreenshot(driver, loc+screenshot_name);
+			
+			
+			throw new SeleniumScriptFailedException (ex);
+			
+		}finally {
+			
+		}
+		
+	}
+	
 	public void findAndClickElement(WebDriver driver,String xpath,String screenshot_name, HTMLOutputWriter html) throws SeleniumScriptFailedException {
 		
 		
@@ -79,18 +174,18 @@ public class SeleniumUtilities {
 			e.click();
 			html.insertRow("Click", xpath, "", 1);
 			System.out.println("Clicked "+xpath+" Sucessfully");
-			
+			Screenshot.takeScreenshot(driver, loc+screenshot_name);
 			
 			
 			
 		}
 		catch(Exception ex)
 		{
-			String loc = "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name;
+			//String loc = "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name;
 			System.out.println("Exception in enter find and click element method -> xpath = "+xpath);
-			Screenshot.takeScreenshot(driver, "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name);
+			//Screenshot.takeScreenshot(driver, "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name);
 			html.insertRow("Click", ex.getMessage(),loc, 0);
-			
+			Screenshot.takeScreenshot(driver, loc+screenshot_name);
 			
 			
 			throw new SeleniumScriptFailedException (ex);
@@ -108,11 +203,11 @@ public class SeleniumUtilities {
 			
 		}
 		catch(Exception e) {
-			String loc = "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name;
-			Screenshot.takeScreenshot(driver, "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name);
+			//String loc = "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name;
+			//Screenshot.takeScreenshot(driver, "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name);
 			html.insertRow("Navigate to", e.getMessage(), loc , 0);
 			System.out.println("Exception in Go to Page method");
-			
+			Screenshot.takeScreenshot(driver, loc+screenshot_name);
 			throw new SeleniumScriptFailedException (e);
 		}finally {
 			
@@ -127,11 +222,13 @@ public class SeleniumUtilities {
 			WebElement e = driver.findElement(By.xpath(xpath));
 			String text = e.getText();
 			html.insertRow("Read", "" , text, 1);
+			Screenshot.takeScreenshot(driver, loc+screenshot_name);
 			return text;
 		}
 		catch(Exception ex) {
-			String loc = "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name;
-			Screenshot.takeScreenshot(driver, loc);
+			//String loc = "C:\\\\Users\\\\i340909\\\\OneDrive - SAP SE\\\\Autothon\\\\screenshots\\\\error_"+screenshot_name;
+		//	Screenshot.takeScreenshot(driver, loc);
+			Screenshot.takeScreenshot(driver, loc+screenshot_name);
 			html.insertRow("Read", ex.getMessage() , loc, 0);
 			System.out.println("Exception in get data from element method xpath ->"+xpath);
 			return "";
--- src/main/java/autothon/autothon/uiAutomation/Steps.java
+++ src/main/java/autothon/autothon/uiAutomation/Steps.java
@@ -4,12 +4,15 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+
+import autothon.autothon.helper.*;
 
 public class Steps extends Thread{
 
@@ -21,12 +24,12 @@ public class Steps extends Thread{
 	WebDriver driver;
 	
 	
-	public Steps(String browser, String device, String status) {
+	public Steps(String browser, String device) {
 		super();
 		this.browser = browser;
 		this.device = device;
 		this.status = status;
-		if(this.device.toLowerCase().equals("android") && this.device.toLowerCase().equals("chrome") ) {
+		if(this.device.toLowerCase().equals("android") && this.browser.toLowerCase().equals("chrome") ) {
 			try {
 				this.driver = androidChromeFactory();
 			} catch (MalformedURLException e) {
@@ -35,7 +38,7 @@ public class Steps extends Thread{
 			}
 		}
 		
-		if(this.device.toLowerCase().equals("windows") && this.device.toLowerCase().equals("chrome") ) {
+		if(this.device.toLowerCase().equals("windows") && this.browser.toLowerCase().equals("chrome") ) {
 			try {
 				this.driver = winChromeFactory();
 			} catch (MalformedURLException e) {
@@ -90,11 +93,61 @@ public class Steps extends Thread{
 
 	public void startExecution() {
 		
+		SeleniumUtilities utl = new autothon.autothon.helper.SeleniumUtilities();
 		 
+		HTMLOutputWriter html = new HTMLOutputWriter(
+				"C:\\Users\\i340909\\OneDrive - SAP SE\\Autothon\\Execution_Output\\" +device, "Logs");
 		//1  open the url
-		
 		//
-		
+		try{
+			
+			html.createTable("Main");
+			utl.goToPage(driver, "https://youtube.com", this.device+"_1.png", html); // go to you tube 1
+			
+			utl.enterInput(driver, "//form/div/div/input", "step-inforum", this.device+"2.png", html); // input in search bar 2
+			utl.findAndClickElement(driver, "//form/button", "dummy", html); // click on search button 
+			
+			WebElement e = utl.findElement(driver, "//a[@href='/user/stepinforum' and @class='yt-simple-endpoint style-scope ytd-channel-renderer']"); //find the searched channel name;
+			
+			if(!e.isDisplayed()) {
+				Thread.sleep(4000);
+			}
+			
+			utl.clickElement(driver, e, this.device+"_3.png");  // click on the channel name 3 
+			
+			e = utl.findElement(driver, "//paper-tab/div[contains(text(),'Videos')]");
+			if(!e.isDisplayed()) {
+				Thread.sleep(4000);
+			}
+			
+			utl.clickElement(driver, e, this.device+"_4.png");
+			
+			//utl.findAndClickElement(driver, "//*[@id=\"tabsContent\"]/paper-tab[2]/div",  this.device+"_4.png", html); // click on videos tab; 4
+			
+			// API call   String  5
+			
+//			WebElement element = utl.findElement(driver, "");  // find the video link from the entire list
+//			utl.scrollDown(driver, element );  // scroll up tp the element 5
+//			 
+//			Screenshot.takeScreenshot(driver, device+"_6.png");
+//			
+//			utl.clickElement(driver, element, device+"_8.png");
+			
+			
+			
+					
+		}
+		catch (Exception e) {
+			System.out.println("Exception E " + e.getMessage());
+			html.closeTable(0);
+			
+		} finally {
+			// html.closeHTML();
+			html.closeHTML();
+			html.cleanup();
+			driver.quit();
+		}
+	
 		
 		
 		
